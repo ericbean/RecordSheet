04f9129ffd5ea9badb425017c3e3aa89cd5868c1
Eric Beanland <eric.beanland@gmail.com>
2016-05-11 04:15:14 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="RecordSheet",
+    version="1.0.dev",
+    author="Eric Beanland",
+    author_email="eric.beanland@gmail.com",
+    description=("Double entry accounting application for small business."),
+    license="GPLv3+",
+    keywords="double entry accounting",
+    packages=['RecordSheet'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        ("License :: OSI Approved :: GNU General Public License v3 or later"
+            " (GPLv3+)"),
+        "Programming Language :: Python :: 3.4"
+    ],
+    entry_points={'console_scripts': ['recordsheet = RecordSheet.webapp:main']},
+    install_requires=['bcrypt==2.0.0',
+                      'Beaker==1.8.0',
+                      'bottle==0.12.9',
+                      'cffi==1.6.0',
+                      'gevent==1.1.1',
+                      'greenlet==0.4.9',
+                      'psycopg2==2.6.1',
+                      'pycparser==2.14',
+                      'six==1.10.0',
+                      'SQLAlchemy==1.0.12']
+)
